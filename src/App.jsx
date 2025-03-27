d920464eb01bd5895f849e21bab08ff916d0414a
Mateo Alvarez <mateoalvarez384gmail.com>
2025-03-27 19:15:04 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import linkeo from "./assets/linkeo-logo.png";
+function App() {
+  return (
+    <div className="title">
+      <img src={linkeo} alt="" />
+      <h1>La interfaz de Linkeo se encuentra actualmente en desarrollo. </h1>
+    </div>
+  );
+}
+
+export default App;
